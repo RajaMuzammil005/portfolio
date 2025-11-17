c11f9df225709544fa03a93a7e75ba9f41731036
Raja Muzammil <muzammilraja533@gmail.com>
2025-11-17 09:29:21 +0500

    Add files via upload

---
+++ Background.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './Background.css'
+import heroImage from './../assets/img.jpeg';
+
+
+
+const Background = () => {
+  return (
+
+    
+
+       <div className="hero-image">
+    <img src={heroImage} alt="hero" />
+  </div>
+    
+    
+  );
+};
+
+export default Background;
